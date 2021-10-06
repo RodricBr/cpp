3c574d6d3194aae265cf4b675abae29f327ca9bb
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-10-06 18:24:18 -0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Hello, world!");
+  return 0;
+}
+
+// Comentário simples
+
+/*
+Comentário multi-linha
+*/
